b54faeedec266a414973d2f5d92fbc70c07a49c7
yasseinbilal <yassein.bilal@gmail.com>
2024-08-14 13:57:43 +0200

    implement query entities

--- src/handlers/ColdStorage.ts
+++ src/handlers/ColdStorage.ts
@@ -2,18 +2,12 @@ import {
   ColdStorageHookContract,
   ColdStorageHook_TimelockExecutedEntity,
   ColdStorageHook_TimelockRequestedEntity,
+  ColdStorageHook_TimelockQueryEntity,
+  ColdStorageHookContract_TimelockRequestedEvent_eventArgs,
+  ColdStorageHookContract_TimelockRequestedEvent_handlerContext,
+  eventLog,
+  ColdStorageHookContract_TimelockExecutedEvent_eventArgs,
 } from "generated";
-
-ColdStorageHookContract.TimelockExecuted.handler(({ event, context }) => {
-  const entity: ColdStorageHook_TimelockExecutedEntity = {
-    id: `${event.transactionHash}_${event.logIndex}`,
-    subAccount: event.params.subAccount,
-    hash: event.params.hash,
-    chainId: event.chainId,
-  };
-
-  context.ColdStorageHook_TimelockExecuted.set(entity);
-});
 
 ColdStorageHookContract.TimelockRequested.handler(({ event, context }) => {
   const entity: ColdStorageHook_TimelockRequestedEntity = {
@@ -25,4 +19,52 @@ ColdStorageHookContract.TimelockRequested.handler(({ event, context }) => {
   };
 
   context.ColdStorageHook_TimelockRequested.set(entity);
+  createTimelockQuery({ event, context });
 });
+
+ColdStorageHookContract.TimelockExecuted.handler(({ event, context }) => {
+  const entity: ColdStorageHook_TimelockExecutedEntity = {
+    id: `${event.transactionHash}_${event.logIndex}`,
+    subAccount: event.params.subAccount,
+    hash: event.params.hash,
+    chainId: event.chainId,
+  };
+
+  context.ColdStorageHook_TimelockExecuted.set(entity);
+  updateTimelockQuery({ event, context });
+});
+
+const createTimelockQuery = ({
+  event,
+  context,
+}: {
+  event: eventLog<ColdStorageHookContract_TimelockRequestedEvent_eventArgs>;
+  context: ColdStorageHookContract_TimelockRequestedEvent_handlerContext;
+}) => {
+  const entity: ColdStorageHook_TimelockQueryEntity = {
+    id: `${event.params.subAccount}_${event.params.hash}`,
+    subAccount: event.params.subAccount,
+    hash: event.params.hash,
+    executeAfter: event.params.executeAfter,
+    chainId: event.chainId,
+    isExecuted: false,
+  };
+
+  context.ColdStorageHook_TimelockQuery.set(entity);
+};
+
+const updateTimelockQuery = ({
+  event,
+  context,
+}: {
+  event: eventLog<ColdStorageHookContract_TimelockExecutedEvent_eventArgs>;
+  context: ColdStorageHookContract_TimelockRequestedEvent_handlerContext;
+}) => {
+  const entity = context.ColdStorageHook_TimelockQuery.get(
+    `${event.params.subAccount}_${event.params.hash}`
+  );
+
+  if (entity) {
+    context.ColdStorageHook_TimelockQuery.set({ ...entity, isExecuted: true });
+  }
+};
--- src/handlers/SmartAccount.ts
+++ src/handlers/SmartAccount.ts
@@ -1,7 +1,13 @@
 import {
   SmartAccountContract,
+  SmartAccountContract_ModuleInstalledEvent_eventArgs,
+  SmartAccountContract_ModuleInstalledEvent_handlerContext,
   SmartAccount_ModuleInstalledEntity,
   SmartAccount_ModuleUninstalledEntity,
+  SmartAccount_ModuleQueryEntity,
+  eventLog,
+  SmartAccountContract_ModuleUninstalledEvent_eventArgs,
+  SmartAccountContract_ModuleUninstalledEvent_handlerContext,
 } from "generated";
 
 SmartAccountContract.ModuleInstalled.handler(({ event, context }) => {
@@ -13,6 +19,7 @@ SmartAccountContract.ModuleInstalled.handler(({ event, context }) => {
   };
 
   context.SmartAccount_ModuleInstalled.set(entity);
+  addModuleToQuery({ event, context });
 });
 
 SmartAccountContract.ModuleUninstalled.handler(({ event, context }) => {
@@ -24,4 +31,47 @@ SmartAccountContract.ModuleUninstalled.handler(({ event, context }) => {
   };
 
   context.SmartAccount_ModuleUninstalled.set(entity);
+  removeModuleFromQuery({ event, context });
 });
+
+const addModuleToQuery = ({
+  event,
+  context,
+}: {
+  event: eventLog<SmartAccountContract_ModuleInstalledEvent_eventArgs>;
+  context: SmartAccountContract_ModuleInstalledEvent_handlerContext;
+}): void => {
+  const module = context.SmartAccount_ModuleQuery.get(
+    `${event.srcAddress}-${event.params.moduleAddress}`
+  );
+
+  if (!module) {
+    const entity: SmartAccount_ModuleQueryEntity = {
+      id: `${event.srcAddress}-${event.params.moduleAddress}`,
+      moduleTypeId: event.params.moduleTypeId,
+      moduleAddress: event.params.moduleAddress,
+      isInstalled: true,
+      chainId: event.chainId,
+    };
+
+    context.SmartAccount_ModuleQuery.set(entity);
+  } else {
+    context.SmartAccount_ModuleQuery.set({ ...module, isInstalled: true });
+  }
+};
+
+const removeModuleFromQuery = ({
+  event,
+  context,
+}: {
+  event: eventLog<SmartAccountContract_ModuleUninstalledEvent_eventArgs>;
+  context: SmartAccountContract_ModuleUninstalledEvent_handlerContext;
+}): void => {
+  const module = context.SmartAccount_ModuleQuery.get(
+    `${event.srcAddress}-${event.params.moduleAddress}`
+  );
+
+  if (module) {
+    context.SmartAccount_ModuleQuery.set({ ...module, isInstalled: false });
+  }
+};
