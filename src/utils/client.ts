1526c3cca12e83867421f7a4ed6ea31a1d9fb5f5
kopy-kat <kopp.konrad@gmail.com>
2024-10-17 15:24:50 +0800

    feat: add chains

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,5 +1,15 @@
 import { Chain, createPublicClient, http } from "viem";
-import { sepolia, optimism, polygon, arbitrum, base } from "viem/chains";
+import {
+  sepolia,
+  optimism,
+  polygon,
+  arbitrum,
+  base,
+  avalanche,
+  gnosis,
+  bsc,
+  baseSepolia,
+} from "viem/chains";
 
 export function getClient(chainId: number) {
   let chain: Chain = optimism;
@@ -8,6 +18,11 @@ export function getClient(chainId: number) {
   if (chainId == 8453) chain = base;
   if (chainId == 137) chain = polygon;
   if (chainId == 42161) chain = arbitrum;
+  if (chainId == 43114) chain = avalanche;
+  if (chainId == 100) chain = gnosis;
+  if (chainId == 56) chain = bsc;
+
+  if (chainId == 84532) chain = baseSepolia;
   if (chainId == 11155111) chain = sepolia;
 
   return createPublicClient({
